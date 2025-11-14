5b3925cc6096b7a19e1c4fb7c52ff65b5c16bc28
nik231 <nnizalov@gmail.com>
2025-11-14 23:10:10 +0000

    start of project

--- ukrcafe/ukrcafe/settings.py
+++ ukrcafe/ukrcafe/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customer_portal',
+    'staff_management',
+    'inventory_management',
+    'staff_management'
 ]
 
 MIDDLEWARE = [
